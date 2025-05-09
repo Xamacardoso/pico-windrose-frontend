7bc4c2bc4232abcf5f2d29b06fb8f895b64bda6f
Xamacardoso <xcardoso1997@gmail.com>
2025-05-09 16:16:23 -0300

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# pico-windrose-frontend
-Ecossistema com frontend em HTML/CSS/JS puro e backend em Flask com WebSocket. Um RP2040 envia dados dos botões A/B e joystick via HTTP, e o frontend recebe esses dados em tempo real via WebSocket.
+# RP2040 Real-Time Dashboard com Flask WebSocket e Frontend em HTML/CSS/JS
+
+Este projeto implementa um ecossistema IoT em nuvem, no qual um microcontrolador RP2040 (Raspberry Pi Pico W) envia dados via HTTP para uma API Flask, que retransmite essas informações em tempo real para este frontend hospedado separadamente, utilizando WebSockets.
+
+## 🔧 Tecnologias Utilizadas
+
+- **RP2040 (Raspberry Pi Pico W)** – Envia os dados dos botões e joystick.
+- **Flask (Python)** – Backend com suporte a WebSocket via Flask-SocketIO.
+- **WebSockets (Socket.IO)** – Comunicação em tempo real com o frontend.
+- **Frontend Vanilla (HTML, CSS, JavaScript)** – Interface dinâmica e interativa.
+- **Hospedagem** – Backend e frontend hospedados separadamente
+
+## 📡 Funcionalidade
+
+- O **RP2040 envia via HTTP** os seguintes dados:
+  - Estado dos botões A e B
+  - Posição X/Y do joystick analógico
+  - Temperatura do sensor interno
+
+- O servidor **Flask** recebe os dados e os **envia para o frontend por WebSocket**.
+
+- O frontend exibe:
+  - Estado dos botões
+  - Coordenadas X/Y do joystick
+  - Temperatura do sensor interno do RP2040
+  - Timestamp da ultima atualização
+  - **Bússola visual interativa indicando a direção do joystick**
+
+## 📂 Estrutura
+
+```
+
+├── index.html      # Página web base
+├── style.css       # Estilização e elementos da bússola
+└── script.js       # Scripts para controle do websocket e da exibilçao de informações
+```
+
+
+
+## 📲 Envio de dados pelo RP2040
+
+O RP2040 pode enviar requisições HTTP para o backend Flask como:
+
+```
+{
+	"btn_a": true,
+	"btn_b": true,
+	"joystick_x": 55,
+	"joystick_y": 55,
+	"temp": 36.5
+}
+```
+
+## 🧭 Interface
+
+A bússola é renderizada com HTML/CSS puro, com agulha rotativa baseada na direção do joystick recebida. Indicadores visuais mostram também o estado dos botões A e B e o valor da temperatura do sensor interno do RP2040.
+
+---
+
+## 💡 Possíveis melhorias
+
+* Autenticação simples via token
+* Logs de dados e histórico
+* Suporte a múltiplos dispositivos conectados
+
+---
+
+## 📜 Licença
+
+Este projeto está licenciado sob a [MIT License](LICENSE).
---
+++ scripts.js
@@ -0,0 +1,73 @@
+const socket = io("https://pico-windrose-backend.onrender.com");
+
+const direcoes = {
+    "Norte": 0,
+    "Nordeste": 45,
+    "Leste": 90,
+    "Sudeste": 135,
+    "Sul": 180,
+    "Sudoeste": 225,
+    "Oeste": 270,
+    "Noroeste": 315
+}
+
+const iniciaisDirecoes = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+
+const needle = document.getElementById('needle');
+const direcoesBussola = document.getElementById('compass-directions');
+
+function atualizarBussola(direcao){
+    let angulo = direcoes[direcao] ?? 0;
+    needle.style.transform = `translate(-50%, -90%) rotate(${angulo}deg)`;
+}
+
+function atualizarInformacoesRecebidas(data) {
+    let direcao = calcularDirecao(data.joystick_x, data.joystick_y);
+
+    document.getElementById("btnA").textContent = data.btn_a ? "Pressionado" : "Solto";
+    document.getElementById("btnB").textContent = data.btn_b ? "Pressionado" : "Solto";
+    document.getElementById("joyX").textContent = data.joystick_x;
+    document.getElementById("joyY").textContent = data.joystick_y;
+    document.getElementById("temp").textContent = data.temp + "° C";
+
+    document.getElementById("direction").textContent = direcao;
+    document.getElementById("timestamp").textContent = data.timestamp;
+    
+    atualizarBussola(direcao);
+}
+
+function calcularDirecao(x, y) {
+    if (x > 66 && y > 66) return "Sudeste";
+    if (x > 66 && y < 33) return "Nordeste";
+    if (x < 33 && y > 66) return "Sudoeste";
+    if (x < 33 && y < 33) return "Noroeste";
+    if (x > 66) return "Leste";
+    if (x < 33) return "Oeste";
+    if (y > 66) return "Sul";
+    if (y < 33) return "Norte";
+    return "Centro";
+}
+
+const chavesDirecoes = Object.keys(direcoes)
+for (let i = 0; i < chavesDirecoes.length; i++){
+    // Wrapper para rotacionar corretamente o indicador de direcao
+    const wrapper = document.createElement('div');
+    wrapper.className = 'direction-wrapper'
+    wrapper.style.transform = `rotate(${direcoes[chavesDirecoes[i]]}deg)`
+
+    // Indicador de direcao
+    const novaDir = document.createElement('span');
+    novaDir.className = 'direction-label'
+    novaDir.textContent = iniciaisDirecoes[i];
+    novaDir.style.transform = `rotate(-${direcoes[chavesDirecoes[i]]}deg)`
+
+    wrapper.appendChild(novaDir);
+    direcoesBussola.appendChild(wrapper);
+}
+
+socket.on("update", (data) => {
+    atualizarInformacoesRecebidas(data);
+    console.log("Recebido:", data);
+    
+});
+
